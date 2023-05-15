709d83d347f7fe8f7dd546f1efa51bef9febd216
Marc Zalik <marc.zalik@gmail.com>
2023-05-15 14:13:58 -0400

    Turn model selection into enum

--- ofrak_ai/ofrak_ai/chatgpt.py
+++ ofrak_ai/ofrak_ai/chatgpt.py
@@ -2,6 +2,7 @@ import os
 import openai
 
 from dataclasses import dataclass
+from enum import StrEnum
 from openai.openai_object import OpenAIObject
 from openai.error import OpenAIError
 from typing import List, Dict, Optional
@@ -9,6 +10,31 @@ from typing import List, Dict, Optional
 from ofrak.model.component_model import ComponentConfig
 from ofrak.model.resource_model import ResourceAttributes
 from ofrak_ai.exponential_backoff import retry_with_exponential_backoff
+
+
+class ModelType(StrEnum):
+    """
+    Models ending in 4-digit long sequences are snapshots of their respective models. These are
+    static versions that receive no updates and will be deprecated 3 months after the newest
+    version is released.
+
+    GPT-4-32k models have a max token value of 32,768 (4x higher than standard GPT-4).
+
+    GPT-4 offers a larger model better optimized for chat than GPT-3.5-turbo as well as a higher max
+    token size (8,192 tokens vs 4,096). GPT-3.5 offers better cost efficiency.
+
+    Note: GPT-4 is currently in limited beta. Make sure that you have access to GPT-4 through
+    OpenAI before attempting to use any of the GPT-4 models with OFRAK AI.
+
+    For more information, visit https://platform.openai.com/docs/models/overview.
+    """
+
+    THREE_FIVE_TURBO = "gpt-3.5-turbo"
+    THREE_FIVE_TURBO_0301 = "gpt-3.5-turbo-0301"
+    FOUR = "gpt-4"
+    FOUR_0314 = "gpt-4-0314"
+    FOUR_32K = "gpt-4-32k"
+    FOUR_32K_0314 = "gpt-4-32k-0314"
 
 
 @dataclass
@@ -25,7 +51,7 @@ class ChatGPTConfig(ComponentConfig):
 
     api_key: Optional[str] = os.getenv("OPENAI_API_KEY")
     api_organization: Optional[str] = os.getenv("OPENAI_ORGANIZATION")
-    model: str = "gpt-3.5-turbo"
+    model: str = ModelType.THREE_FIVE_TURBO
     system_message: Optional[str] = None
     temperature: float = 1.0
 
