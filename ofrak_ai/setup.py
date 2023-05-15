e73402b59831c889dfe83e6673f3d73db52caf5a
Marc Zalik <marc.zalik@gmail.com>
2023-05-15 15:43:43 -0400

    Remove license

--- ofrak_ai/setup.py
+++ ofrak_ai/setup.py
@@ -11,7 +11,6 @@ class egg_info_ex(egg_info):
         if not self.distribution.have_run.get("install", True):
             # `install` command is in progress, copy license
             self.mkpath(self.egg_info)
-            self.copy_file("LICENSE", self.egg_info)
 
         egg_info.run(self)
 
@@ -49,21 +48,14 @@ setuptools.setup(
     download_url="https://github.com/redballoonsecurity/ofrak",
     project_urls={
         "Documentation": "https://ofrak.com/docs/",
-        "Community License": "https://github.com/redballoonsecurity/ofrak/blob/master/LICENSE",
-        "Commercial Licensing Information": "https://ofrak.com/license/",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
-        "License :: Other/Proprietary License",
-        "License :: Free To Use But Restricted",
-        "License :: Free For Home Use",
         "Topic :: Security",
         "Typing :: Typed",
     ],
     python_requires=">=3.8",
-    license="Proprietary",
-    license_files=["LICENSE"],
     cmdclass={"egg_info": egg_info_ex},
     entry_points={
         "ofrak.packages": ["ofrak_pkg = ofrak"],
